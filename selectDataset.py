0f699fa89bba80d1a3b38763ce19c56f8202f4f8
louiskyee <bolin8017@gmail.com>
2023-09-22 14:27:27 +0800

    modify the data structure when selecting files

--- selectDataset.py
+++ selectDataset.py
@@ -20,6 +20,7 @@ class selectDataset(object):
         self.matrix = np.full((0, 0), -1.0, dtype=float)
         self.fileNames = []
         self.choosed_files = []
+        self.available_files = np.full(0, 1)
         self.min_values = np.full(0, np.inf)
         self.nextIdx = -1
         self.maxValue = -np.inf
@@ -31,7 +32,6 @@ class selectDataset(object):
         self.parameter_parser()
         self.get_file_list()
         self.get_choosed_files()
-        self.write_hash_dict()
         self.write_choosed_files_to_txt()
 
     def parameter_parser(self):
@@ -75,44 +75,29 @@ class selectDataset(object):
             for file in self.choosed_files:
                 f.write(f"{file}\n")
 
-    def __get_next_file(self, rowIdx):
-        # Initialize the maximum value and the next index
-        nextIdx = -1
-        maxValue = -np.inf
-
-        # Iterate over the column indices of the matrix
-        for colIdx, value in enumerate(self.matrix[rowIdx]):
-            # Check if this column needs to be considered
-            if value >= 0.0 and self.fileNames[colIdx] not in self.choosed_files:
-                # Find the minimum value in this
-                self.min_values[colIdx] = min(self.min_values[colIdx], value)
-                if maxValue < self.min_values[colIdx]:
-                    maxValue = self.min_values[colIdx]
-                    nextIdx = colIdx
-        self.min_values[nextIdx] = -1
-
-        return nextIdx
-
     def __calculate_weight(self):
         curFile:str = random.sample(self.fileNames, 1)[0]  # random select first file
         self.choosed_files = [curFile]
-        self.min_values[self.fileNames.index(curFile)] = -1
+        curFileIdx = self.fileNames.index(curFile)
+        self.available_files[curFileIdx] = 0
+        self.min_values[curFileIdx] = -1
         while len(self.choosed_files) < self.number_of_choose_files:
-            rowIdx = self.fileNames.index(curFile)
             hashValue1 = self.hash_dict[curFile]
+            maxValue = -np.inf
+            nextIdx = -1
             for fileName in self.fileNames:
                 if fileName in self.choosed_files or fileName == curFile:
                     continue
 
                 hashValue2 = self.hash_dict[fileName]
-                colIdx = self.fileNames.index(fileName)
-
-                # Check if the weight has been calculated before
-                if self.matrix[rowIdx, colIdx] == -1.0:
-                    weight = tlsh.diff(hashValue1, hashValue2)
-                    self.matrix[rowIdx, colIdx] = weight
-                    self.matrix[colIdx, rowIdx] = weight
-            nextIdx = self.__get_next_file(rowIdx)
+                iterFileIdx = self.fileNames.index(fileName)
+                if self.min_values[iterFileIdx] == np.inf:
+                    self.min_values[iterFileIdx] = min(self.min_values[iterFileIdx], tlsh.diff(hashValue1, hashValue2))
+                # Find the maximum value in this
+                if maxValue < self.min_values[iterFileIdx]:
+                    maxValue = self.min_values[iterFileIdx]
+                    nextIdx = iterFileIdx
+            self.min_values[nextIdx] = -1
             curFile = self.fileNames[nextIdx]
             self.choosed_files.append(curFile)
 
@@ -122,12 +107,12 @@ class selectDataset(object):
             hashValue = tlsh.hash(open(filePath, 'rb').read())
             fileName = os.path.basename(filePath)
             self.hash_dict[fileName] = hashValue
-
+        self.write_hash_dict()
         self.number_of_choose_files = len(self.hash_dict) // self.timesLess
         print(f"number of choosed files = {self.number_of_choose_files}")
         n = len(self.hash_dict)
         self.fileNames:list = list(self.hash_dict.keys())
-        self.matrix = np.full((n, n), -1.0, dtype=float)
+        self.available_files = np.full(n, 1)
         self.min_values = np.full(n, np.inf)
         self.__calculate_weight()
 
